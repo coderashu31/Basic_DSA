ebae411e07a72042d810e8275d66766cb38a9736
coderashu31 <ashishrkl31@gmail.com>
2024-12-21 21:03:12 +0530

    arrays_01

---
+++ 01_Arrays/01_SmallestNumber.cpp
@@ -0,0 +1,48 @@
+// Problem Statement: Given an array, we have to find the smallest element in the array.
+
+// Examples:
+// Example 1:
+// Input: arr[] = {2,5,1,3,0};
+// Output: 0
+// Explanation: 0 is the smallest element in the array. 
+
+// Example2: 
+// Input: arr[] = {8,10,5,7,9};
+// Output: 5
+// Explanation: 5 is the smallest element in the array
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// method 1: find the min and return. 
+// int findSmallestElement(vector<int> &arr){
+//     sort(arr.begin(),arr.end());
+//     return arr[0];
+// }
+
+
+
+
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n); // Declare a vector of size n
+
+    // Take input for each element
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = findSmallestElement(arr);
+
+    cout << "The smallest element of the array is: " << smallest << endl;
+
+    return 0;
+
+}
