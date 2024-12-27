8acf349d1ee2afc3fe2b35fe0e76531844be4f7a
coderashu31 <ashishrkl31@gmail.com>
2024-12-27 20:32:39 +0530

    Resolving merge conflicts

---
+++ 01_Arrays/26_ArraySubset.cpp
@@ -0,0 +1,18 @@
+// Check if array is subset of another array
+
+// Example 1:
+// Input: arr1[]= [1,3,4,5,2]
+//        arr2[]= [2,4,3,1,7,5,15]
+// Output: arr1[] is a subset of arr2[]
+
+// Example 2:
+// Input: arr1[]= [1,3,4,5,2]
+//        arr2[]= [4,5,2]
+// Output: arr1[] is not a subset of arr2[]
+
+// Example 3:
+// Input: arr1[]= [1,3,4,5,2]
+//        arr2[]= [11,12,13,15,16]
+// Output: arr1[] is not a subset of arr2[]
+
+
